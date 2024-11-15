85e865f03a5f5ef54b9b125635667a5bc9e100f0
Ellenz123 <ellenz759@deltalearns.ca>
2024-11-15 10:32:55 -0800

    I am finished.

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+#! python3
+
+# SD Computing Studies Assignment
+fname="Mr"
+Iname="Yang"
+print(f"The value of fname is {fname}")
+print(f"The value of Iname is {Iname}")
---
+++ assignment3.py
@@ -0,0 +1,7 @@
+#! python3
+
+# SD Computing Studies Assignment
+
+value1=10
+value2=value1
+print(f"The value of value2 is {value1}")
